19770ffab999753f7023ecbc0c5751533a61162c
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-24 14:16:35 +0530

    Update script.py

--- script.py
+++ script.py
@@ -37,6 +37,5 @@ Note :Please note that only english type fonts are supported in hardmux other sc
 
 Welcome to **{bot_name}**\n
 I Can help you to Merge Subtitle in Video\n
-
 **Made With 💕 By @Tellybots_4u
 """
