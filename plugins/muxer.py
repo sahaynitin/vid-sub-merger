b6c7477fda6bccd04cd590c4d74a26cd53b3248b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-24 13:45:58 +0530

    Update muxer.py

--- plugins/muxer.py
+++ plugins/muxer.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from helper_func.progress_bar import progress_bar
 from helper_func.dbhelper import Database as Db
 from helper_func.mux import softmux_vid, hardmux_vid
+from pyrogram.errors import FloodWait, UserNotParticipant
 from config import Config
 from plugins.forcesub import handle_force_subscribe
 import time
