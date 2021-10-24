3710fb212f0228ab92df637316e5b95dadfe3717
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-10-24 10:44:55 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,33 @@
+class Script:
+  ABOUT = """
+🤖 **Name:** {bot_name}
+
+📝 **Language:** [Python](https://www.python.org)
+
+📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+
+📡 **Hosted On:** [Heroku](https://heroku.com)
+
+🧑‍💻 **Developer:** [Safone](https://t.me/ImSafone)
+
+👥 **Support Group:** [SafoTheBot](https://t.me/safothebot)
+
+📢 **Updates Channel:** [Ｓ１ ＢＯＴＳ](https://t.me/AsmSafone)
+"""
+
+  HELP_USER = """
+This is **{bot_name}**
+
+This Bot Can Download Files & Videos From Mega Links & Upload To Telegram. Just Send Any Mega.nz Link & See The Magic. You Can Also Add or Change Caption: Just Select An Uploaded File/ Video or Forward Me Any Telegram File & Then Write The Text You Want To Be Caption On The File As A Reply To That File & The Text You Wrote Will Be Attached As Caption 😁! 
+
+**Made With ❤️ By @AsmSafone! 👑**
+"""
+
+  START_TEXT = """
+👋🏻 **Hi** {user_mention},
+
+I'm **{bot_name}**
+I Can Download Files & Videos From Mega.nz Links & Upload To Telegram. Please Check Help To Learn More 😉!
+
+**Maintained By: {bot_owner}**❤️!
+"""
