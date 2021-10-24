cebfce94b4a9138390204a30295cdc00506a1cb7
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-10-24 11:48:22 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -43,7 +43,7 @@ async def handle_force_subscribe(bot, message):
     except Exception:
         await bot.send_message(
             chat_id=message.from_user.id,
-            text="Something Went Wrong. Contact My [Support Group](https://t.me/safothebot).",
+            text="Something Went Wrong. Contact My [Support Group](https://t.me/Tellybots_4u).",
             parse_mode="markdown",
             disable_web_page_preview=True,
             reply_to_message_id=message.message_id,
